5ade2e7fe3e5c9adee711fc5498b77dc1b71af60
gettingwings <74932161+gettingwings@users.noreply.github.com>
2024-01-27 15:45:50 +0530

    Update Move-Position.js

--- Move-Position.js
+++ Move-Position.js
@@ -14,37 +14,7 @@ AFRAME.registerComponent("move", {
   },
 });
 
-AFRAME.registerComponent("camera-zoom-out", {
-  schema: {
-    moveZ: { type: "number", default: 10 },
-  },
 
-  tick: function () {
-    this.data.moveZ = this.data.moveZ + 0.01;
-
-    var pos = this.el.getAttribute("position");
-
-    pos.z = this.data.moveZ;
-
-    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
-  },
-});
-
-AFRAME.registerComponent("fall-down", {
-  schema: {
-    moveY: { type: "number", default: 0 },
-  },
-  tick: function () {
-
-    window.addEventListener("click", (e) => {   
-
-      this.data.moveY = this.data.moveY - 0.0001;
-
-    });
-    var pos = this.el.getAttribute("position");
-    pos.y = pos.y + this.data.moveY;
-    this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
-  }
 });
 
 
